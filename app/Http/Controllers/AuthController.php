777f1f9fb3d1433fc5b433803af7e0830da70db7
emmy70 <emmanuelusman701@gmail.com>
2021-06-05 07:39:43 +0100

    Rest-api-auth completed

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -21,6 +21,7 @@ class AuthController extends Controller
             'password' => bcrypt($fields['password'])
 
         ]);
+
         $token = $user->createToken('myapptoken')->plainTextToken;
 
         $response = [
@@ -28,6 +29,41 @@ class AuthController extends Controller
             'token' => $token
         ];
         return response($response, 201);
+
+    }
+
+    public function login(Request $request){
+        $fields = $request->validate([
+        'email' => 'required|string',
+        'password' => 'required|string'
+        ]);
+
+        // Check email
+        $user = User::where('email', $fields['email'])->first();
+
+        // Chek password
+        if(!$user || !Hash::check($fields['password'], $user->password)) {
+            return response([
+                'message' => 'Bad credentials'
+            ], 401);
+        }
+
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+        return response($response, 201);
+
+    }
+
+    public function logout(Request $request){
+        auth()->user()->tokens()->delete();
+
+        return[
+            'message' => 'Logged out'
+        ];
 
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
-use App\Http\Controllers\ProductController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,27 +13,23 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
 //Route::resource('products', ProductController::class);
 
 //Public routes
 Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{id}', [ProductController::class, 'show']);
 Route::get('/products/search/{name}', [ProductController::class, 'search']);
-
-
-
-
-
 
 //Protected routes
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('/products', [ProductController::class, 'store']);
     Route::put('/products/{id}', [ProductController::class, 'update']);
-    Route::delete('/products/{id }', [ProductController::class, 'distroy']);
-
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 
 
 });
